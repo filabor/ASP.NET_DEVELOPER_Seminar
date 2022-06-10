7279cf5103402daea0cc7f16cdf8d9a759cfc58b
filabor <94072806+filabor@users.noreply.github.com>
2022-06-10 15:26:02 +0200

    Added extensions for AspNetUsers

--- ASP.NET_seminarski_rad/CMS_seminar/Data/ApplicationDbContext.cs
+++ ASP.NET_seminarski_rad/CMS_seminar/Data/ApplicationDbContext.cs
@@ -1,10 +1,24 @@
 ﻿using CMS_seminar.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace CMS_seminar.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(150)]
+        public string Address { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
