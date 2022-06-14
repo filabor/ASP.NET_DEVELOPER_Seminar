7db255262d269a7cc5c786063d4cbd8bc97d97a9
filabor <94072806+filabor@users.noreply.github.com>
2022-06-14 11:29:35 +0200

    Added generic interface

---
+++ ASP.NET_seminarski_rad/CMS_seminar/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CMS_seminar.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        public T GetById(int id);
+        public void CreateNew(T obj);
+        public void Delete(int id);
+        public void Update(T obj);
+    }
+}
