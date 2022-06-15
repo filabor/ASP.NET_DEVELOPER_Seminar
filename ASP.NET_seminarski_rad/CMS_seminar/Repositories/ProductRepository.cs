4864379b2af90b79383699432440b8e3906d79a3
filabor <94072806+filabor@users.noreply.github.com>
2022-06-15 17:41:06 +0200

    Added Product repository

---
+++ ASP.NET_seminarski_rad/CMS_seminar/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using CMS_seminar.Data;
+using CMS_seminar.Interfaces;
+using CMS_seminar.Models;
+
+namespace CMS_seminar.Repositories
+{
+    public class ProductRepository : IGenericRepository<Product>
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void CreateNew(Product new_product)
+        {
+            _context.Products.Add(new_product);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var product_to_remove = _context.Products.SingleOrDefault(p => p.Id == id);
+            _context.Remove(product_to_remove);
+
+            _context.SaveChanges();
+        }
+
+    }
+}
