f46f407bf1a38431b912b19b3975a7833c49deeb
filabor <94072806+filabor@users.noreply.github.com>
2022-06-29 13:55:54 +0200

    Added filter for products by category

--- ASP.NET_seminarski_rad/CMS_seminar/Controllers/HomeController.cs
+++ ASP.NET_seminarski_rad/CMS_seminar/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CMS_seminar.Models;
+﻿using CMS_seminar.Interfaces;
+using CMS_seminar.Models;
 using CMS_seminar.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,11 +10,15 @@ namespace CMS_seminar.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ProductService _productService;
+        private readonly IGenericRepository<ProductCategory> _productCategoryRepository;
+        private readonly IGenericRepository<Category> _categoryRepositoy;
 
-        public HomeController(ILogger<HomeController> logger, ProductService productService)
+        public HomeController(ILogger<HomeController> logger, ProductService productService, IGenericRepository<ProductCategory> productCategoryRepository, IGenericRepository<Category> categoryRepository)
         {
             _logger = logger;
             _productService = productService;
+            _productCategoryRepository = productCategoryRepository;
+            _categoryRepositoy = categoryRepository;
         }
 
         public IActionResult Index()
@@ -26,9 +31,24 @@ namespace CMS_seminar.Controllers
             return View();
         }
 
-        public IActionResult Product()
+        public IActionResult Product(int? category_id)
         {
-            var products = _productService.GetAllProducts();
+            var products = new List<Product>();
+
+            if (category_id != null)
+            {
+                products = _productService.GetAllProducts()
+                    .Where(p => _productCategoryRepository.GetAll()
+                    .Where(pc => pc.CategoryId == category_id)
+                    .Select(pc => pc.ProductId).ToList().Contains(p.Id)).ToList();
+            }
+            else
+            {
+                products = _productService.GetAllProducts().ToList();
+            }
+
+            ViewBag.Categories = _categoryRepositoy.GetAll();
+
             return View(products);
         }
 
