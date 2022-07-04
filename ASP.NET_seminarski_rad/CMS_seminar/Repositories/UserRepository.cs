a1214e91d8516463dc297d8c66cd70b958bb6e2f
filabor <94072806+filabor@users.noreply.github.com>
2022-07-04 19:53:38 +0200

    Added User repository

---
+++ ASP.NET_seminarski_rad/CMS_seminar/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using CMS_seminar.Data;
+using CMS_seminar.Interfaces;
+
+namespace CMS_seminar.Repositories
+{
+    public class UserRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+           return _context.Users.ToList();
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void CreateNew(ApplicationUser new_user)
+        {
+            _context.Users.Add(new_user);
+            _context.SaveChanges();
+        }
+
+        public void Update(ApplicationUser user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var user_to_remove = _context.Users.SingleOrDefault(u => u.Id == id);
+            _context.Users.Remove(user_to_remove);
+            _context.SaveChanges();
+        }
+    }
+}
