b007141fe7ff3fd7774d313e291aad169ecddb93
filabor <94072806+filabor@users.noreply.github.com>
2022-09-06 18:57:15 +0200

    Added seeding migration and updated appsettings.json file

--- ASP.NET_seminarski_rad/CMS_seminar/Data/ApplicationDbContext.cs
+++ ASP.NET_seminarski_rad/CMS_seminar/Data/ApplicationDbContext.cs
@@ -30,5 +30,219 @@ namespace CMS_seminar.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<CMS_seminar.ViewModels.UserViewModel>? UserViewModel { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Title = "Kategorija 1"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Title = "Kategorija 2"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Title = "Kategorija 3"
+                }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Proizvod 1",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 8,
+                    Price = 1500.00M,
+                    ImageName = "Image_1.jpg"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Proizvod 2",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 15,
+                    Price = 1698.99M,
+                    ImageName = "Image_2.jpg"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Proizvod 3",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 20,
+                    Price = 2598.59M,
+                    ImageName = "Image_3.jpg"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Proizvod 4",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 8,
+                    Price = 3599.99M,
+                    ImageName = "Image_4.jpg"
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Proizvod 5",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 16,
+                    Price = 1400.00M,
+                    ImageName = "Image_5.jpg"
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Proizvod 6",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 6,
+                    Price = 1462.82M,
+                    ImageName = "Image_6.jpg"
+                },
+                new Product
+                {
+                    Id = 7,
+                    Title = "Proizvod 7",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 23,
+                    Price = 4578.99M,
+                    ImageName = "Image_7.jpg"
+                },
+                new Product
+                {
+                    Id = 8,
+                    Title = "Proizvod 8",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 34,
+                    Price = 1680.00M,
+                    ImageName = "Image_8.jpg"
+                },
+                new Product
+                {
+                    Id = 9,
+                    Title = "Proizvod 9",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 28,
+                    Price = 4000.00M,
+                    ImageName = "Image_9.jpg"
+                },
+                new Product
+                {
+                    Id = 10,
+                    Title = "Proizvod 10",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+                    Quantity = 4,
+                    Price = 3495.00M,
+                    ImageName = "Image_10.jpg"
+                }
+            );
+
+            modelBuilder.Entity<ProductCategory>().HasData(
+                new ProductCategory
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    ProductId = 1
+                },
+                new ProductCategory
+                {
+                    Id = 2,
+                    CategoryId = 2,
+                    ProductId = 1
+                },
+                new ProductCategory
+                {
+                    Id = 3,
+                    CategoryId = 1,
+                    ProductId = 2
+                },
+                new ProductCategory
+                {
+                    Id = 4,
+                    CategoryId = 3,
+                    ProductId = 2
+                },
+                new ProductCategory
+                {
+                    Id = 5,
+                    CategoryId = 2,
+                    ProductId = 3
+                },
+                new ProductCategory
+                {
+                    Id = 6,
+                    CategoryId = 3,
+                    ProductId = 4
+                },
+                new ProductCategory
+                {
+                    Id = 7,
+                    CategoryId = 1,
+                    ProductId = 5
+                },
+                new ProductCategory
+                {
+                    Id = 8,
+                    CategoryId = 3,
+                    ProductId = 5
+                },
+                new ProductCategory
+                {
+                    Id = 9,
+                    CategoryId = 2,
+                    ProductId = 6
+                },
+                new ProductCategory
+                {
+                    Id = 10,
+                    CategoryId = 1,
+                    ProductId = 7
+                },
+                new ProductCategory
+                {
+                    Id = 11,
+                    CategoryId = 2,
+                    ProductId = 8
+                },
+                new ProductCategory
+                {
+                    Id = 12,
+                    CategoryId = 3,
+                    ProductId = 8
+                },
+                new ProductCategory
+                {
+                    Id = 13,
+                    CategoryId = 1,
+                    ProductId = 9
+                },
+                new ProductCategory
+                {
+                    Id = 14,
+                    CategoryId = 1,
+                    ProductId = 10
+                },
+                new ProductCategory
+                {
+                    Id = 15,
+                    CategoryId = 2,
+                    ProductId = 10
+                },
+                new ProductCategory
+                {
+                    Id = 16,
+                    CategoryId = 3,
+                    ProductId = 10
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ ASP.NET_seminarski_rad/CMS_seminar/Data/Migrations/20220906150422_SeedingDatabase.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS_seminar.Data.Migrations
+{
+    public partial class SeedingDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Kategorija 1" },
+                    { 2, "Kategorija 2" },
+                    { 3, "Kategorija 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageName", "Price", "Quantity", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_1.jpg", 1500.00m, 8m, "Proizvod 1" },
+                    { 2, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_2.jpg", 1698.99m, 15m, "Proizvod 2" },
+                    { 3, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_3.jpg", 2598.59m, 20m, "Proizvod 3" },
+                    { 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_4.jpg", 3599.99m, 8m, "Proizvod 4" },
+                    { 5, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_5.jpg", 1400.00m, 16m, "Proizvod 5" },
+                    { 6, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_6.jpg", 1462.82m, 6m, "Proizvod 6" },
+                    { 7, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_7.jpg", 4578.99m, 23m, "Proizvod 7" },
+                    { 8, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_8.jpg", 1680.00m, 34m, "Proizvod 8" },
+                    { 9, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_9.jpg", 4000.00m, 28m, "Proizvod 9" },
+                    { 10, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..", "Image_10.jpg", 3495.00m, 4m, "Proizvod 10" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "CategoryId", "ProductId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 1 },
+                    { 3, 1, 2 },
+                    { 4, 3, 2 },
+                    { 5, 2, 3 },
+                    { 6, 3, 4 },
+                    { 7, 1, 5 },
+                    { 8, 3, 5 },
+                    { 9, 2, 6 },
+                    { 10, 1, 7 },
+                    { 11, 2, 8 },
+                    { 12, 3, 8 },
+                    { 13, 1, 9 },
+                    { 14, 1, 10 },
+                    { 15, 2, 10 },
+                    { 16, 3, 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
